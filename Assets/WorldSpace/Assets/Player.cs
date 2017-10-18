3b3aca624314e96671bb21f8ec55b3694d533eaf
AKIHIRO NAGASHIMA <akihiro.nagashima@gmail.com>
2017-10-18 17:22:22 +0900

    チャプター２ 3D空間UI

---
+++ Assets/WorldSpace/Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public float duration = 0.4f;
+
+	public void MoveX (float distance)
+	{
+		StartCoroutine(MoveXCoroutine(distance));
+	}
+
+	IEnumerator MoveXCoroutine(float distance)
+	{
+		float currentX = transform.position.x;
+		float targetX = currentX + distance;
+		float startTime = Time.time;
+		float t = 0;
+
+		while (t < 1.0f)
+		{
+			t = (Time.time - startTime) / duration;
+			transform.position = new Vector3(Mathf.SmoothStep(currentX, targetX, t), 0, 0);
+			yield return 0;
+		}
+	}
+}
---
+++ Assets/WorldSpace/Assets/PlayerName.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerName : MonoBehaviour
+{ 
+	[SerializeField]
+	private Transform target;
+
+	private Vector3 screenPoint;
+
+	void LateUpdate()
+	{
+		screenPoint = Camera.main.WorldToScreenPoint(target.position);
+
+		transform.position = new Vector3(screenPoint.x, screenPoint.y, 0);
+	}
+}
